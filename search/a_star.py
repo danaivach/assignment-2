e85da5c6d210bf95f3bb19499f25c7e18eaedc52
danaivach <danai.vacht@gmail.com>
2022-03-01 18:42:00 +0100

    Add template astar

--- search/a_star.py
+++ search/a_star.py
@@ -144,7 +144,7 @@ def astar_search(
     # The cost of reaching the initial state is 0
     state_cost = {task.initial_state: 0}
 
-    # An increasing value to prefer the value first inserted in the list of nodes
+    # An increasing value to prefer the value first inserted in the heap of nodes
     # if the cost of two or more nodes is the same
     node_tiebreaker = 0
 
@@ -153,18 +153,18 @@ def astar_search(
     init_h = heuristic(root)
 
     # The nodes to be expanded. Each node will be stored along with the 
-    # 1) estimated heuristing values f and h for reaching the goal state from the 
+    # 1) estimated heuristic values f and h for reaching the goal state from the 
     # state in node, and 2) the node_tiebreaker value
     open = []
 
     # Add the root node in the heap of nodes to be expanded ("open").
-    # The root node is stored along along with 1) the estimated heuristing values 
+    # The root node is stored along along with 1) the estimated heuristic values 
     # f and h for reaching the goal state from the state in node, and 2) the 
     # node_tiebreaker value. f is calculated based on the callable parameter
     # make_open_entry, i.e. based on the type of the A* search algorithm.
     #
     # The node is stored in a heap so that nodes are stored based on their 
-    # estimated cost. See more here: https://pythontic.com/algorithms/heapq/heappush
+    # estimated cost! See more here: https://pythontic.com/algorithms/heapq/heappush
     heapq.heappush(open, make_open_entry(root, init_h, node_tiebreaker))
     logging.info("Initial h value: %f" % init_h)
 
@@ -178,42 +178,45 @@ def astar_search(
 
     # Iterate over the heap as long as the heap contains items to pop
     while open:
-        # Step 1
+
+        # ---- Step 1 ----
         # Pop the next node from the heap (i.e. the next node with the lowest estimated cost)
         # This is the node that will be expanded in this round
         # HINT: Use the function heappop() of the heapq module: https://pythontic.com/algorithms/heapq/heappop
-        (f, h, _tie, pop_node) = heapq.heappop(open)
+        (f, h, _tie, pop_node) = (None, float("inf"), None, None) # update this line to implement step 1 as instructed
 
         # Update the best cost value
         if h < besth:
             besth = h
-            logging.debug("Found new best h: %d after %d expansions" % (besth, counter))
+            logging.info("Found new best h: %d after %d expansions" % (besth, counter))
 
-        # Get the state in the node to be expanded
-        pop_state = pop_node.state
+        # ---- Step 2 ----
+        # Get the state in the SearchNode node to be expanded. See searchspace.py
+        pop_state = None 
 
-        # Get the cost g of the node to be expanded (i.e. the path length, i.e. the number of applied 
-        # operators for reaching the state in node)
-        pop_g = pop_node.g
+        # ---- Step 3 ----
+        # Get the cost g of the SearchNode node to be expanded (i.e. the path length, i.e. the number of applied 
+        # operators for reaching the state in node) See searchspace.py
+        pop_g = None
 
-        # Step 2 
-        # Only expand the node if its associated cost (g value) is the lowest
-        # cost known for this state. Otherwise we already found a cheaper
+        # ---- Step 4 ----
+        # Only expand the node if its associated cost (pop_g value) is the lowest
+        # cost known for this state (pop_state). Otherwise, we have already found a cheaper
         # path after creating this node and hence can disregard it.
         # HINT 1: Compare the state cost of pop_state with pop_g. If they are
         #         equal, proceed to the next step. Else continue to a new round.
         # HINT 2: The state cost of pop_state can be retrieved from the 
         #         state_cost dictionary (see before loop)
-        if state_cost[pop_state] == pop_node.g:
+       
         
-        # If the state cost of pop_state is equal to pop_g:
-            # Step 3
-            # Increase the expansions counter
-            expansions += 1
+        # If the state cost of pop_state is equal to pop_g (step 2):
+            # ---- Step 5 ----
+            # Increase the expansions counter and optionally print it
 
-            # Step 4
+
+            # ---- Step 6 ----
             # If the goal of the task has been reach in the state
-            # of the node, then extract the solution and return it.
+            # of the node, then extract the solution and return it!
             # HINT 1: The variable pop_state holds the state in the node
             # HINT 2: Use a Task method that you implemented to check if
             #         the goal of the task has been reached at the the state 
@@ -221,67 +224,53 @@ def astar_search(
             # HINT 3: Use the SearchNode method extract_solution() to extract
             #         and return the solution to the task (in case the goal has
             #         been achieved)
-            if task.goal_reached(pop_state):
-                logging.info("Goal reached. Start extraction of solution.")
-                logging.info("%d Nodes expanded" % expansions)
-                return pop_node.extract_solution()
+           
 
-            # Step 4 
+            # ---- Step 7 ----
             # Create and add each neighbor node of the node to the heap if it is worth exploring
             # HINT 1: You can create neighbor nodes, using the SearchNode method 
             #         make_child_node()
             # HINT 2: Use a Task method that you implemented to retrieve 
             #         all the possible neighbor (next) states that can be reached
-            #         after the state (pop_state) in the node. Information stored in the 
+            #         after the state (i.e. pop_state) in the node. Information stored in the 
             #         retrieved neighbor states, can be used to create a neighbor 
             #         node with make_child_node()
-            # Step 4.1: Retrieve and iterate over the possible neighbor (next) states that can 
-            #           be reached after the state (pop_state) in the node. 
+            # Step 7.1: Retrieve and iterate over the possible neighbor (next) states that can 
+            #           be reached after the state (i.e. pop_state) in the node. 
             # For every neighbor state:
-            #         Step 4.2: Create a neighbor node with make_chid_node()
-            #         Step 4.3: Calculate the h cost of the neighbor node using the callable
-            #                   parameter "heuristic" (see above how the h cost was calculated 
-            #                   for the root node)
-            #         Step 4.4: If h is equal to infinite continue to a new round (the next neighbor state) 
-            #                   (Step 4.2). You don't need to care about states that can't reach the goal
-            #         Step 4.5: Compare the state cost of the neighbor node's state with the cost g
-            #                   of the neighbor node to see if the neighbor node is worth expanding. 
-            #                   If the state of the neighbor node is not in cost_state (i.e. the state 
-            #                   hasn't been reached before), we should expand the node to be able to
-            #                   reach the state. Go to Step 5. 
-            #                   Else if the state of the neighbor node is in cost_state (i.e. the state 
-            #                   has been reached before), and the cost g of neighbor node is smaller than 
-            #                   the stored cost in cost_state, we should expand the node to reach the
-            #                   state in a cheaper way. Go to Step 5.
-            #                   Else continue to a new round (the next neighbor state) (Step 4.2), since we can 
-            #                   already reach the state in a cheaper way.
+                # Step 7.2: Create a neighbor node with make_child_node()
+                # Step 7.3: Calculate the h cost of the neighbor node using the callable
+                #           parameter "heuristic" (see above how the h cost was calculated 
+                #           for the root node)
+                # Step 7.4: If h is equal to infinite continue to a new round (the next neighbor state) 
+                #           (Step 7.2). You don't need to care about states that can't reach the goal.
+                # Step 7.5: Else, compare the state cost of the neighbor node's state with the cost g
+                #           of the neighbor node to see if the neighbor node is worth expanding! 
+                #           If the state of the neighbor node is not in cost_state (i.e. the state 
+                #           hasn't been reached before), we should expand the node to be able to
+                #           reach the state. Go to Step 8. 
+                #           Else, if the state of the neighbor node is in cost_state (i.e. the state 
+                #           has been reached before), and the cost g of neighbor node is smaller than 
+                #           the stored cost in cost_state, we should expand the node to reach the
+                #           state in a cheaper way. Go to Step 8.
+                #           Else, continue to a new round (the next neighbor state) (Step 7.2), since we can 
+                #           already reach the state in a cheaper way discovered in the past.
 
-            #         If one of the conditions in 4.5 holds:
-            #           Step 5: Add the neighbor node to the heap
-            #                   Step 5.1: Increase node_tiebreaker by 1
-            #                   Step 5.2: Store the g cost of the neighbor node to the state_cost dictionary
-            #                   Step 5.3: Add the neighbor node in the heap of nodes to be expanded ("open").
-            #                             The neighbor node is stored along with 1) the estimated heuristing values 
-            #                             f and h for reaching the goal state from the state in node, and 2) the 
-            #                             node_tiebreaker value. f is calculated based on the callable parameter
-            #                             make_open_entry, i.e. based on the type of the A* search algorithm.
-            #
-            #                             The node is stored in a heap so that nodes are ordered based on their 
-            #                             estimated cost. See more here: https://pythontic.com/algorithms/heapq/heappush
-            #                             See also above, how the root node was stored in the heap.
-            for op, succ_state in task.get_successor_states(pop_state):
-                succ_node = searchspace.make_child_node(pop_node, op, succ_state)
-                h = heuristic(succ_node)
-                if h == float("inf"):
-                    # don't bother with states that can't reach the goal anyway
-                    continue
-                old_succ_g = state_cost.get(succ_state, float("inf"))
-                if succ_node.g < old_succ_g:
-                    # We either never saw succ_state before, or we found a
-                    # cheaper path to succ_state than previously.
-                    node_tiebreaker += 1
-                    heapq.heappush(open, make_open_entry(succ_node, h, node_tiebreaker))
-                    state_cost[succ_state] = succ_node.g
+
+                # If one of the conditions in 7.5 holds:
+                    # ---- Step 8 ----
+                    # Add the neighbor node to the heap
+                    # Step 8.1: Increase node_tiebreaker by 1
+                    # Step 8.2: Store the g cost of the neighbor node to the state_cost dictionary
+                    # Step 8.3: Add the neighbor node in the heap of nodes to be expanded ("open").
+                    #           The neighbor node is stored along with 1) the estimated heuristic values 
+                    #           f and h for reaching the goal state from the state in node, and 2) the 
+                    #           node_tiebreaker value. f is calculated based on the callable parameter
+                    #           make_open_entry, i.e. based on the type of the A* search algorithm.
+                    #
+                    #           The node is stored in a heap so that nodes are ordered based on their 
+                    #           estimated cost. See more here: https://pythontic.com/algorithms/heapq/heappush
+                    #           See also above, how the root node was stored in the heap.
 
         # Increase the counter by 1
         counter += 1
